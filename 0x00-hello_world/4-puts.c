29a182341d39f64aee831bac68b03f9b1d26bff8
olaholunuga <olaholunuga@gmail.com>
2021-06-17 13:14:16 -0700

    write C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - function prints string
+*
+* Return: main
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}       
