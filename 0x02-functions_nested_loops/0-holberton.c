d96bf420d0a20ee4298f7d49ad313897dd8e4863
olaholunuga <olaholunuga@gmail.com>
2021-06-22 08:55:47 -0700

    how to print a string without printf

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * main - functions prints string
+ *
+ * Return: 0
+ */
+int main(void)
+{
+_putchar("Holberton");
+_putchar('\n');
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# 0x02-functions_nested_loops
+- 0-holberton.c _ prints a string without the use of printf
