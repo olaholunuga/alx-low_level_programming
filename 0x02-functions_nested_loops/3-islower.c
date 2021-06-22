8399aa2e6bdea5134983173064f25a8ba224c48a
olaholunuga <olaholunuga@gmail.com>
2021-06-22 12:30:59 -0700

    checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
-* _islower - Retturn lower case character
-*
-* Return: 1
-*/
+ * _islower - returns 1 if c is lowercase, 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
