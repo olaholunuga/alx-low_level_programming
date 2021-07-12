b6855c86ea993a4abd740f0bab4c63c9f72d8232
olaholunuga <olaholunuga@gmail.com>
2021-07-12 10:17:13 -0700

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - funtion
+ * @argc: unused variable
+ * @argv: used variable
+ * @i: variable
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+
+return 0;
+}
