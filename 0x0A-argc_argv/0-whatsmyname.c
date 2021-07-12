feb107ceb7694bc45d6e46b8c77235eb98780921
olaholunuga <olaholunuga@gmail.com>
2021-07-12 08:46:48 -0700

    program that prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- *
+ * main - funtion to print the first variable
+ * @argc: unused variale
+ * @argv: used variable
+ * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
 printf("%s\n", argv[0]);
-return 0;
+return (0);
 }
