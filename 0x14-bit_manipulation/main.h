0774d92b2fb662d6467c5b7f539e423f9fb35974
olaholunuga <olaholunuga@gmail.com>
2021-08-05 13:16:58 -0700

    all fixed

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+ * File: main.h
+ * AUthor: olunuga olaoluwa ayomide
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-bit_manipulation directory.
+ */
+
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
-
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
